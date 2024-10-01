56553cffd3b75ba6274e2b9a428538eaafef0225
karlascalas <83492414+karlascalas@users.noreply.github.com>
2024-10-01 08:46:06 +0530

    "Optional Commit"

--- src/main/java/com/core/example/java9/features/OptionalExample.java
+++ src/main/java/com/core/example/java9/features/OptionalExample.java
@@ -9,4 +9,20 @@ public class OptionalExample {
         optional.ifPresentOrElse(value -> System.out.println("Value is present: "+value), () -> System.out.println("Value is not present"));
     }
 
+    public void optional_Empty(){
+        Optional<String> optional = Optional.empty();
+        Optional<String> result = optional.or(()-> Optional.of("Default value"));
+        System.out.println(result.get());
+    }
+
+    public void optional_stream(){
+        Optional<String> optional =  Optional.of("Hello");
+        optional.stream().forEach(System.out::println);
+    }
+
+    public void optional_orElseThrow(){
+        Optional<String> optional = Optional.empty();
+        String value = optional.orElseThrow(() -> new IllegalArgumentException("Value is not present")); 
+    }
+
 }
